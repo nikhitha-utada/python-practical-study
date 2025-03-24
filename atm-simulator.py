47f7c354670bfff039445faeb59049f8c708e9a3
nikhitha-utada <nikhithautada2003@gmail.com>
2025-03-24 20:44:37 +0530

    atm-simulator

---
+++ atm-simulator.py
@@ -0,0 +1,14 @@
+# 🛠️ Project Requirements:
+# Set an initial account balance (e.g., ₹10,000).
+
+# Display menu options:
+# 1. Check Balance
+# 2. Deposit Money
+# 3. Withdraw Money
+# 4. Exit
+
+# Perform transactions based on user input:
+# If the user selects withdrawal, ensure the balance is sufficient.
+# If the user selects deposit, add the amount to the balance.
+# If the user selects check balance, display the current balance.
+# If the user selects exit, stop the program.
